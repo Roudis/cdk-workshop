d3b4cc45d96841671a6168044005656496c50f52
roudis <alkisroudis@gmail.com>
2026-02-25 09:33:04 +0100

    Lalmbds working

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,11 +1,15 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda"
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_22_X,
+      code: Code.fromAsset("lambda"),
+      handler: "hello.handler"
+    })
   }
 }
